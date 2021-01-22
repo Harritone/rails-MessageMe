f85af18c25d6a891b8a60d0dae7da48dc6dfee09
Harritone <yourrasik@gmail.com>
2021-01-22 22:28:57 +1000

    Add messages controller, add message from UI

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,12 @@
+class MessagesController < ApplicationController
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      redirect_to root_path
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
