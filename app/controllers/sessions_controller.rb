be5bc022a3227206c5c1d825dcc055e8f4a88852
Harritone <yourrasik@gmail.com>
2021-01-22 21:42:59 +1000

    Add restrict views at controller layer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
     @user = User.new
@@ -23,6 +24,13 @@ class SessionsController < ApplicationController
   end
   
   private
+
+  def logged_in_redirect 
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:username, :password)
