6da96fffc1613ab8a216e0b503ab10e5f6d67bf8
Harritone <yourrasik@gmail.com>
2021-01-23 14:41:39 +1000

    created custom scope and styled input box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ scroll_bottom = function () {
 };
 
 submit_message = function () {
-  $('#message_body').on('submit', function (e) {
+  $('#message_body').on('keydown', function (e) {
     if (e.keyCode == 13) {
       $('button').click();
       e.target.value = '';
